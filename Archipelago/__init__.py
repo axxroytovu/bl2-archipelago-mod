2cd53affd7b9102298ac2e3838426cdf0bd135ef
Sam Zimmerman <samzimmerman@me.com>
2023-11-06 14:30:00 -0800

    Working chat messages

--- Archipelago/__init__.py
+++ Archipelago/__init__.py
@@ -44,10 +44,9 @@ def _DisplayGameMessage(message: str, subtitle: str, duration: float = 5) -> Non
     )
 
 def send_chat():
-    unrealsdk.Log("Key Pressed")
-    #chat_box = TextInputBox("Message", "", True)
-    #chat_box.OnSubmit=next(iter(_pubsub._core_websocket.values())).send_chat(msg)
-    #chat_box.Show()
+    chat_box = TextInputBox("Message", "", True)
+    chat_box.OnSubmit=next(iter(_pubsub._topic_websockets.values())).send_chat
+    chat_box.Show()
     
 
 class Archipelago(ModMenu.SDKMod):
@@ -60,11 +59,10 @@ class Archipelago(ModMenu.SDKMod):
     Types: ModMenu.ModTypes = ModMenu.ModTypes.Gameplay
     
     Keybinds = [
-        ModMenu.Keybind("Chat Window", "F3", OnPress=lambda: unrealsdk.Log("Key Pressed")),
+        ModMenu.Keybind("Chat Window", "F3", OnPress=send_chat),
     ]
 
     Status: str = "<font color=\"#ff0000\">Not Connected</font>"
-    SettingsInputs: Dict[str, str] = { "Enter": "Connect" }
 
     Options: Sequence[ModMenu.Options.Base] = ( _saved_server_port, _saved_player_name )
 
@@ -106,55 +104,27 @@ class Archipelago(ModMenu.SDKMod):
         pc.OnlineSub.ContentInterface.ObjectPointer.ReadDownloadableContentList(controller_id)
 
 
-    def LoggedIn(self) -> None:
+    def Enable(self) -> None:
+        ModMenu.HookManager.RegisterHooks(self)
+        ModMenu.NetworkManager.RegisterNetworkMethods(self)
+        box_server = TextInputBox("Archipelago Connection String", f"{_saved_player_name.CurrentValue}:{Passcode}@{_saved_server_port.CurrentValue}", True)
+        def OnSubmit(msg):
+            unrealsdk.Log(msg)
+            name_code, _saved_server_port.CurrentValue = msg.split("@")
+            _saved_player_name.CurrentValue, Passcode = name_code.split(":")
+            _pubsub.OpenTopic(msg)
+            self.Server = _pubsub._topic_websockets[msg]
+        box_server.OnSubmit = OnSubmit
+        box_server.Show()
         unrealsdk.Log("Logged In")
         self.Status = "<font color=\"#00ff00\">Logged In</font>"
-        self.Description = f"{Archipelago.Description}\n\nCurrently logged in as: {_saved_player_name.CurrentValue}"
-        self.SettingsInputs = { "Delete": "Disconnect" }
         self._update_mod_menu()
 
-    def LoggedOut(self) -> None:
+    def Disable(self) -> None:
+        ModMenu.HookManager.RemoveHooks(self)
+        ModMenu.NetworkManager.UnregisterNetworkMethods(self)
+        _pubsub.CloseAll()
         self.Status = "<font color=\"#ff0000\">Logged Out</font>"
-        self.Description = Archipelago.Description
-        self.SettingsInputs = { "Enter": "Connect" }
-        self._update_mod_menu()
-
-    def LoginExpired(self) -> None:
-        self.Status = "<font color=\"#ff0000\">Login Expired</font>"
-        self.Description = (
-            Archipelago.Description + 
-            "\n\nYour login has expired, please login again."
-        )
-        self.SettingsInputs = { "Enter": "Reconnect" }
-        self._update_mod_menu()
-        _DisplayGameMessage(
-            "Archipelago Connection Expired",
-            "Please log in again to continue using Archipelago"
-        )
-
-    def ConnectionFailed(self) -> None:
-        self.Status = "<font color=\"#ffff00\">Connection Failed</font>"
-        self.Description = (
-            Archipelago.Description + 
-            "\n\nError connecting to the Archipelago server."
-        )
-        self.SettingsInputs = { "Enter": "Reconnect" }
-        self._update_mod_menu()
-        _DisplayGameMessage(
-            "Archipelago Connection Failure",
-            "Archipelago Mod will not be functional."
-        )
-
-    def MissingPermissions(self) -> None:
-        mod_names = ", ".join(mod.Name for mod in _mods_missing_permissions)
-
-        self.Status = "<font color=\"#ffff00\">Missing Permissions</font>"
-        self.Description = (
-            f"{Archipelago.Description}\n\n"
-            f"Currently logged in as: {UserName}\n\n"
-            f"Missing permissions required by mods: {mod_names}"
-        )
-        self.SettingsInputs = { "Enter": "Reconnect", "Delete": "Disconnect" }
         self._update_mod_menu()
 
     def parse_inputs(self, address, message):
@@ -171,42 +141,11 @@ class Archipelago(ModMenu.SDKMod):
                 self.Server.reconnect()
             elif messageType == "CONNECTED":
                 unrealsdk.Log("CONNECTED")
-                self.LoggedIn()
             elif messageType == "PRINTJSON":
                 for text in content['data']:
                     _DisplayGameMessage(text['text'], "")
             #else:
             #    unrealsdk.Log(content)
-
-    def SettingsInputPressed(self, action: str) -> None:
-        unrealsdk.Log(action)
-        #pc = unrealsdk.GetEngine().GamePlayers[0].Actor
-        #self._mod_menu_item = pc.GetFrontendMovie().MarketplaceMovie.GetSelectedObject()
-
-        if action == "Connect":
-            
-            box_server = TextInputBox("Archipelago Connection String", f"{_saved_player_name.CurrentValue}:{Passcode}@{_saved_server_port.CurrentValue}", True)
-            def OnSubmit(msg):
-                unrealsdk.Log(msg)
-                name_code, _saved_server_port.CurrentValue = msg.split("@")
-                _saved_player_name.CurrentValue, Passcode = name_code.split(":")
-                _pubsub.OpenTopic(msg)
-                self.Server = _pubsub._topic_websockets[msg]
-            box_server.OnSubmit = OnSubmit
-            box_server.Show()
-
-        elif action == "Disconnect":
-            _pubsub.CloseAll()
-            self.LoggedOut()
-
-        """
-        elif action == "Re-Login":
-            _authorization.Logout()
-            _authorization.InitiateLogin(_registered_scopes)
-
-        elif action == "Reconnect":
-            _authorization.Validate(force=True)
-        """
 
     def __init__(self):
         _pubsub.MessageCallback = self.parse_inputs
